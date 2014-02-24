4d30e5cb0085a52800f0ec5c35c636528a3d3467
Julien Colmonts <julien.colmonts@gmail.com>
2014-02-24 16:37:22 +0100

    Welcome view done

--- src/com/needsreal/social/MainActivity.java
+++ src/com/needsreal/social/MainActivity.java
@@ -1,8 +1,10 @@
 package com.needsreal.social;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class MainActivity extends Activity
@@ -12,6 +14,9 @@ public class MainActivity extends Activity
 	protected void onCreate (Bundle savedInstanceState)
 	{
 		super.onCreate (savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView (R.layout.activity_main);
 	}
 
