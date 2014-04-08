fab477e55b5207e5592edc5b5219b6fdec74eb19
Matthieu Baerts <matttbe@gmail.com>
2014-04-08 16:31:21 +0200

    Added support of Network

--- src/com/needsreal/social/activities/MapMainActivity.java
+++ src/com/needsreal/social/activities/MapMainActivity.java
@@ -6,6 +6,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -22,6 +23,16 @@ public class MapMainActivity extends FragmentActivity implements LocationListene
 {
 	private LocationManager locationManager;
 	private GoogleMap map;
+
+	private boolean isUsingGPS = false;
+	private boolean isUsingNetwork = false;
+
+	private static final int GPS_MIN_TIME_UPDATE = 2 * 60 * 1000; // 2min
+	private static final int NETWORK_MIN_TIME_UPDATE = 5 * 60 * 1000; // 5min
+	private static final int MIN_DISTANCE_UPDATE = 10; // 10 meters
+	private static final float ZOOM_INIT = 13;
+	private static final float ZOOM_GPS = 14;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState)
 	{
@@ -49,10 +60,10 @@ public class MapMainActivity extends FragmentActivity implements LocationListene
 		// .icon (BitmapDescriptorFactory.defaultMarker
 		// (BitmapDescriptorFactory.HUE_AZURE))
 		
-		FragmentManager fm = getFragmentManager();
-		fm.beginTransaction()
-		          .hide(getFragmentManager ().findFragmentById (R.id.searchblock))
-		          .commit();
+		FragmentManager fm = getFragmentManager ();
+		fm.beginTransaction ()
+				.hide (getFragmentManager ()
+						.findFragmentById (R.id.searchblock)).commit ();
 
 	}
 
@@ -68,57 +79,104 @@ public class MapMainActivity extends FragmentActivity implements LocationListene
 		// Si le GPS est disponible, on s'y abonne
 		if (locationManager.isProviderEnabled (LocationManager.GPS_PROVIDER))
 			registerGPS ();
+		else if (locationManager.isProviderEnabled (LocationManager.NETWORK_PROVIDER))
+			registerNetwork ();
 		else
 			Toast.makeText (this, "GPS not available", Toast.LENGTH_LONG)
 					.show ();
+
+		// center around the last know location
+		Location lastKnowLocation = null;
+		lastKnowLocation = locationManager
+				.getLastKnownLocation (LocationManager.GPS_PROVIDER);
+		if (lastKnowLocation == null)
+		{
+			lastKnowLocation = locationManager
+					.getLastKnownLocation (LocationManager.NETWORK_PROVIDER);
+			// TODO: if NULL => getLastLocation from settings
+		}
+
+		if (lastKnowLocation != null)
+			moveCamera (lastKnowLocation, ZOOM_INIT);
 	}
 
 	@Override
 	public void onPause ()
 	{
 		super.onPause ();
-		unregisterGPS ();
+		unregisterLocation ();
 	}
 
 	private void registerGPS ()
 	{
+		Log.d ("GPS", "GPS registered");
+		isUsingGPS = true;
 		locationManager.requestLocationUpdates (LocationManager.GPS_PROVIDER,
-				5000, 10, this);
+				GPS_MIN_TIME_UPDATE, MIN_DISTANCE_UPDATE, this);
 	}
 
-	private void unregisterGPS ()
+	private void registerNetwork ()
 	{
+		Log.d ("GPS", "Network registered");
+		isUsingNetwork = true;
+		locationManager.requestLocationUpdates (LocationManager.NETWORK_PROVIDER,
+				NETWORK_MIN_TIME_UPDATE, MIN_DISTANCE_UPDATE, this);
+	}
+
+	private void unregisterLocation ()
+	{
+		Log.d ("GPS", "unregistered");
+		isUsingGPS = isUsingNetwork = false;
 		locationManager.removeUpdates (this);
+	}
+
+	/**
+	 * Move camera to a location with a custom zoom
+	 * @param location the center position
+	 * @param zoom between 2 and 21, see CameraUpdateFactory.newLatLngZoom
+	 */
+	private void moveCamera (final Location location, float zoom)
+	{
+		final LatLng latLng = new LatLng (location.getLatitude (),
+				location.getLongitude ());
+		Log.d ("GPS", "move camera to " + latLng.latitude + " " + latLng.longitude);
+		map.moveCamera (CameraUpdateFactory.newLatLngZoom (latLng, zoom));
 	}
 
 	@Override
 	public void onLocationChanged (final Location location)
 	{
-		final LatLng latLng = new LatLng (location.getLatitude (),
-				location.getLongitude ());
-		map.moveCamera (CameraUpdateFactory.newLatLngZoom (latLng, 15));
-
+		moveCamera (location, ZOOM_GPS);
 	}
 
 	@Override
 	public void onProviderDisabled (final String provider)
 	{
+		Log.d ("GPS", "provider disabled " + provider);
 		// GPS no longer available
-		if (provider.equals ("gps"))
-			unregisterGPS ();
+		if ((isUsingGPS && provider.equals (LocationManager.GPS_PROVIDER))
+				|| (isUsingNetwork
+						&& provider.equals (LocationManager.NETWORK_PROVIDER)))
+			unregisterLocation ();
 	}
 
 	@Override
 	public void onProviderEnabled (final String provider)
 	{
+		Log.d ("GPS", "provider enabled " + provider);
 		// yeah, GPS is back
-		if (provider.equals ("gps"))
+		if (isUsingGPS && provider.equals (LocationManager.GPS_PROVIDER))
 			registerGPS ();
+		else if (isUsingNetwork
+				&& provider.equals (LocationManager.NETWORK_PROVIDER))
+			registerNetwork ();
 	}
 
 	@Override
 	public void onStatusChanged (final String provider, final int status,
 			final Bundle extras)
-	{}
+	{
+		Log.d ("GPS", "status changed " + provider + ": " + status);
+	}
 
 }
