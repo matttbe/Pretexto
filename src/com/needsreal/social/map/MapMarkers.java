e26083e1261cc60c0697cbfd822a9875ddeb34ab
Matthieu Baerts <matttbe@gmail.com>
2014-04-09 17:35:36 +0200

    Marker: implement updateMarker with markers from ServerRequests

--- src/com/needsreal/social/map/MapMarkers.java
+++ src/com/needsreal/social/map/MapMarkers.java
@@ -1,27 +1,36 @@
 package com.needsreal.social.map;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import android.location.Location;
 import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.needsreal.social.search.SearchItem;
+import com.needsreal.social.server.Server;
 
 public class MapMarkers
 {
 	private GoogleMap map;
+	private HashMap<String, SearchItem> hashMap;
 
 	public MapMarkers (GoogleMap map)
 	{
 		this.map = map;
+		this.hashMap = new HashMap<String, SearchItem> ();
 	}
 
 	private ArrayList<SearchItem> getMarkersForLocation (Location location,
 			float radius)
 	{
 		Log.d ("Markers", "getMarkers: " + location + " " + radius);
-		return null;
+		ArrayList<SearchItem> markers = Server
+				.getServerMgr ()
+				.getServerRequests ()
+				.getMarkersForLocation (location.getLatitude (),
+						location.getLongitude (), radius);
+		return markers;
 	}
 
 	private ArrayList<SearchItem> getMarkersForLocation ()
@@ -32,32 +41,37 @@ public class MapMarkers
 	}
 
 	/**
-	 * Get Markers for my location if I can be located
-	 */
-	private ArrayList<SearchItem> getMarkersForMyLocation ()
-	{
-		Location myLocation = map.getMyLocation ();
-		if (myLocation == null) // no signal...
-			return null;
-
-		Object[] locationInfo = MapCamera.radiusDistanceCovered (map);
-		return getMarkersForLocation (map.getMyLocation (),
-				(Float) locationInfo[0]);
-	}
-
-	/**
 	 * Update the list of markers in the list
 	 * @param bForceUpdate force requests on the server TODO
 	 */
-	public void updateMarkers (Location location, boolean bForceUpdate)
+	public void updateMarkers (boolean bForceUpdate)
 	{
-		// TODO
-		getMarkersForLocation ();
-		getMarkersForMyLocation ();
-		SearchItem testSearchItem = new SearchItem (0, 50.6684991, 4.621698,
-				"Beau gosse", "Viens me rencontrer, moi, Julien le beau gosse");
+		ArrayList<SearchItem> items = getMarkersForLocation ();
+		for (SearchItem item : items)
+		{
+			// if the item already exists, update the position
+			if (hashMap.containsKey (item.getHash ()))
+			{
+				Log.d ("Markers", "An old item");
+				SearchItem inHashItem = hashMap.get (item.getHash ());
+				if (inHashItem.hasMoved (item.getLocation ()))
+				{
+					Log.d ("Markers",
+							"An old item: the position has changed ; from "
+									+ inHashItem.getLocation () + " to "
+									+ item.getLocation ());
+					inHashItem.updateLocation (item.getLocation ());
+				}
+			}
+			else
+			{
+				Log.d ("Markers", "An new item");
+				item.addMarkerToMap (map);
+				hashMap.put (item.getHash (), item);
+			}
 
-		testSearchItem.addMarkerToMap (map);
+		}
+
 		// .icon (BitmapDescriptorFactory.defaultMarker
 		// (BitmapDescriptorFactory.HUE_AZURE))
 	}
