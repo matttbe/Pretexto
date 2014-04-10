b3379053cd35ef46a0d9ce84120ca8eb93772137
Matthieu Baerts <matttbe@gmail.com>
2014-04-10 15:32:39 +0200

    Added UserFields enum

---
+++ src/com/needsreal/social/profile/UserFields.java
@@ -0,0 +1,19 @@
+package com.needsreal.social.profile;
+
+public enum UserFields
+{
+	HASH,
+	NICKNAME,
+	FIRSTNAME,
+	LASTNAME,
+	SHORTDESC,
+	DESC,
+	ACTIPRO,
+	CITY,
+	POSTCODE,
+	COUNTRY,
+	MOOD,
+	INTERESTS,
+	NEEDS,
+	CONTACTS
+}
