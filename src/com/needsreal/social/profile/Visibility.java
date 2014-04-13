07af1408ffcfba442aa37a6a621c2ba345ce3f2a
Matthieu Baerts <matttbe@gmail.com>
2014-04-13 17:58:22 +0200

    Visibility: manage basic cases:
    Associate a setting (key-value) with a boolean.
    List all settings with the value (e.g.:John) and the boolean (ON-OFF)

--- src/com/needsreal/social/profile/Visibility.java
+++ src/com/needsreal/social/profile/Visibility.java
@@ -1,7 +1,109 @@
 package com.needsreal.social.profile;
 
+/**
+ * The user can specify the visibility of some details of its profile and save
+ * the selection with a name.
+ *
+ * What do we need?
+ *  * Associate a setting (key-value) with a boolean.
+ *  * List all settings with the value (e.g.:John) and the boolean (ON-OFF)
+ *  * Save/Load settings
+ *  * Send/Receive settings
+ *
+ * Special cases:
+ *  * Some settings can be specific, e.g. the precision
+ *  * Some settings contain a list, e.g. the contact list. We could enable only
+ *    some of them
+ */
 public class Visibility
 {
-	public String name;
-	
+	private String name;
+
+	private VisibilityObject[] settings;
+
+	public Visibility (String name)
+	{
+		this.name = name;
+
+		UserFields[] fields = UserFields.values ();
+		settings = new VisibilityObject[fields.length];
+
+		for (int i = 0; i < fields.length; i++)
+			settings[i] = new VisibilityObject (fields[i]);
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName ()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName (String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the settings
+	 */
+	public VisibilityObject[] getSettings ()
+	{
+		return settings;
+	}
+
+	/**
+	 * Save to the DataBase
+	 */
+	public void saveToDB ()
+	{
+		// TODO
+		/*
+		 * for (VisibilityObject item : settings) { setBoolean
+		 * (item.field.getMethodName (), item.value); }
+		 */
+	}
+
+	/**
+	 * Load values from the DataBase
+	 */
+	public void loadFromDB ()
+	{
+		// TODO
+		/*
+		 * for (VisibilityObject item : settings) { item.value = getBoolean
+		 * (item.field.getMethodName (), item.value); }
+		 */
+	}
+
+	/**
+	 * send to the Server
+	 */
+	public void sendToServer ()
+	{
+		// TODO
+	}
+
+	/**
+	 * Load values from the server
+	 */
+	public void loadFromServer ()
+	{
+		// TODO
+	}
+
+	public class VisibilityObject
+	{
+		public UserFields field;
+		public boolean value;
+
+		public VisibilityObject (UserFields field)
+		{
+			this.field = field;
+		}
+	}
 }
