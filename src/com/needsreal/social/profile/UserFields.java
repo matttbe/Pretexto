4b1ffdb50d80a8beaaad57dc05e8f81c3e44ba29
Matthieu Baerts <matttbe@gmail.com>
2014-04-13 17:56:16 +0200

    UserFields: link a Field (e.g. HASH) with a method (e.g. getHash())

--- src/com/needsreal/social/profile/UserFields.java
+++ src/com/needsreal/social/profile/UserFields.java
@@ -1,21 +1,65 @@
 package com.needsreal.social.profile;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import android.util.Log;
+
 public enum UserFields
 {
-	HASH,
-	NICKNAME,
-	FIRSTNAME,
-	LASTNAME,
-	SHORTDESC,
-	DESC,
-	ACTIPRO,
-	CITY,
-	POSTCODE,
-	COUNTRY,
-	POSITION,
-	PRECISION,
-	MOOD,
-	INTERESTS,
-	NEEDS,
-	CONTACTS
+	HASH ("getHash"),
+	NICKNAME ("getNickname"),
+	FIRSTNAME ("getFirstName"),
+	LASTNAME ("getLastName"),
+	SHORTDESC ("getShortDescription"),
+	DESC ("getDescription"),
+	ACTIPRO ("getActivityPro"),
+	CITY ("getCity"),
+	POSTCODE ("getPostcode"),
+	COUNTRY ("getCountry"),
+	POSITION ("getPosition"),
+	PRECISION (null),
+	MOOD ("getMood"),
+	INTERESTS ("getInterests"),
+	NEEDS ("getNeeds"),
+	CONTACTS ("getContacts");
+
+	private final String methodName;
+
+	UserFields (String method)
+	{
+		this.methodName = method;
+	}
+
+	public String getMethodName ()
+	{
+		return methodName;
+	}
+
+	public Object invoke (AbstractUser user)
+	{
+		Method method;
+		try
+		{
+			method = user.getClass ().getMethod (methodName);
+		} catch (NoSuchMethodException e)
+		{
+			Log.e ("User", e.getMessage ());
+			return null;
+		}
+		try
+		{
+			return method.invoke (user);
+		} catch (IllegalAccessException e)
+		{
+			Log.e ("User", e.getMessage ());
+		} catch (IllegalArgumentException e)
+		{
+			Log.e ("User", e.getMessage ());
+		} catch (InvocationTargetException e)
+		{
+			Log.e ("User", e.getMessage ());
+		}
+		return null;
+	}
 }
