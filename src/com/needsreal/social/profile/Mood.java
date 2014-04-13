a46715894b52af6ee0a4ce0638d1d19f6cdaffe0
Matthieu Baerts <matttbe@gmail.com>
2014-04-13 19:54:26 +0200

    Mood: added 'getDrawable'

--- src/com/needsreal/social/profile/Mood.java
+++ src/com/needsreal/social/profile/Mood.java
@@ -1,8 +1,22 @@
 package com.needsreal.social.profile;
 
+import com.needsreal.social.R;
+
 public enum Mood
 {
-	HAPPY,
-	SAD,
+	HAPPY (R.drawable.logo_menu),
+	SAD   (R.drawable.logo_menu);
 	// TODO
+
+	private int resID;
+
+	Mood (int resID)
+	{
+		this.resID = resID;
+	}
+
+	public int getDrawable ()
+	{
+		return resID;
+	}
 }
